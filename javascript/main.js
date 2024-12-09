6a64a4dd02fb450b90be63fceb214d02c2d6446c
MUDAVATH SIDDU <136369091+Siddumudavath@users.noreply.github.com>
2024-12-09 19:12:39 +0530

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+
+document.querySelectorAll(".btn h2").forEach(function(x) {
+    x.style.textAlign = "center";
+    x.style.marginTop="50px"
+    x.style.color="black"
+  });
+  
+function fun(colorchnage) {
+  
+    document.body.style.backgroundColor = colorchnage;
+}
+let head=document.getElementById("heading2")
+head.style.textAlign="center"
+head.style.lineHeight="50px"
+head.style.marginTop="50px"
